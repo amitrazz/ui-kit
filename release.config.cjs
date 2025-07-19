c605ba3f6e17f8c60c8088a6a07d4fa749d429aa
Amit Kumar <noreply@amitrazz.in>
2025-07-19 22:49:37 +0530

    feat: init commit

--- release.config.cjs
+++ release.config.cjs
@@ -1,19 +1,16 @@
 module.exports = {
-  branches: ['master'],
   repositoryUrl: 'https://github.com/amitrazz/ui-kit',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-    }],
-    ['@semantic-release/npm', {
-      npmPublish: true,
-    }],
-    ['@semantic-release/git', {
-      assets: ['CHANGELOG.md', 'package.json'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    }],
-    '@semantic-release/github',
-  ],
+    branches: ['master'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+        {
+          assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+          message: 'release: v${nextRelease.version} \n\n${nextRelease.notes}\n\n[skip ci]'
+        }
+
+    ],
 };
